f9cbec981d7ee629dd27cebf4831307882828c98
gnazarkin <gleb.nazarkin@gmail.com>
2015-05-28 17:15:52 -0400

    Navigation styling and icons

--- app/controllers/navigation.js
+++ app/controllers/navigation.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-	needs: ['application'],
+	needs: ['application', 'admin-navigation'],
 	title: "Navigation Bar",
 
 	model: Ember.A([
