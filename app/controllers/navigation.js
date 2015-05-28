6b740e634e6055a38f6a8efb9bf79141adf8f296
gnazarkin <gleb.nazarkin@gmail.com>
2015-05-28 13:46:19 -0400

    Basic navigation styling

--- app/controllers/navigation.js
+++ app/controllers/navigation.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+	needs: ['application'],
 	title: "Navigation Bar",
 
 	model: Ember.A([
+		Ember.Object.create({title: "Edit Profile", route: ""}),
+		Ember.Object.create({title: "Account Settings", route: ""}),
 		Ember.Object.create({title: "Properties", route: 'properties.index'}),
-		Ember.Object.create({title: "New Property", route: 'properties.new'})
+		Ember.Object.create({title: "Beacons", route: 'properties.new'}),
+		Ember.Object.create({title: "Microapps", route: 'properties.new'}),
+		Ember.Object.create({title: "Users", route: 'properties.new'})
 	])
 });
