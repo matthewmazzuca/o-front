282dd239a13aa1ffdd9c0c21caf09e80c2764f91
gnazarkin <gleb.nazarkin@gmail.com>
2015-05-30 14:26:00 -0400

    Can save promotion

---
+++ app/controllers/highlight/edit.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	actions: {
+		save: function() {
+			var model = this.get('model');
+			var self = this;
+			model.save().then(function() {
+				self.transitionToRoute('properties.index');
+			}, function() {
+				alert('save error');
+			});
+		}
+	}
+});
