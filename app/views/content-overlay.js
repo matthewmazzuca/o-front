ae1bbdec8b2d6bce616b8f3c4ac23a284a7307a5
gnazarkin <gleb.nazarkin@gmail.com>
2015-06-02 13:45:24 -0400

    content-overlay to partial

--- app/views/content-overlay.js
+++ app/views/content-overlay.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
 	//clicking overlay toggles nav
+	templateName: 'partials/content-overlay',
 	click: function(e) {
 		this.get('controller').send('toggleNav');
 	}
