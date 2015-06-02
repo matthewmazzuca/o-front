193a54d61fbe8b5b22d06ddaddf79877fdbc0c48
gnazarkin <gleb.nazarkin@gmail.com>
2015-06-02 13:49:32 -0400

    Moved editable field to partial

--- app/views/editable-field.js
+++ app/views/editable-field.js
@@ -2,4 +2,5 @@ import EditableFieldMixin from "../mixins/editable-field";
 import Ember from 'ember';
 
 export default Ember.View.extend(EditableFieldMixin, {
+	templateName: 'partials/editable-field',
 });
