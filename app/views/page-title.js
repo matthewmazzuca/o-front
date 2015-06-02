927a44dee5ef6cea6b5c48f09d65a86952a63bce
gnazarkin <gleb.nazarkin@gmail.com>
2015-06-02 13:47:03 -0400

    page-title to partial

--- app/views/page-title.js
+++ app/views/page-title.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
 	needs: ['application'],
-	templateName: 'page-title',
+	templateName: 'partials/page-title',
 
 	actions: {
 		back: function() {
