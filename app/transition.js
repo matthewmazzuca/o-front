5c15b67f34ae725c789ff32452594a1b74c54283
gnazarkin <gleb.nazarkin@gmail.com>
2015-06-10 13:09:39 -0400

    Animate nav

--- app/transition.js
+++ app/transition.js
@@ -3,4 +3,21 @@ export default function() {
   this.transition(
     this.use('toLeft')
   );
+
+  //match the down arrow
+  this.transition(
+    this.fromRoute('property.show'),
+    this.toRoute('property.details'),
+    this.use('toDown')
+  );
+
+  //nav state
+  this.transition(
+    this.hasClass('nav-visible'),
+    this.use('toRight', {duration: 300}),
+
+    //set true so reverse animations work
+    this.toModel(true),
+    this.reverse('toLeft', {duration: 300})
+  );
 };
