8329495721957d9552fee4a10366d00fb3ef80cc
gnazarkin <gleb.nazarkin@gmail.com>
2015-06-10 15:39:46 -0400

    Tidy user

--- app/initializers/current-user.js
+++ app/initializers/current-user.js
@@ -6,6 +6,7 @@ export default {
   initialize: function(container, application) {
     var store, user, device;
 
+    //use device uuid to identify user
     store = container.lookup('store:main');
     if (window.device) {
       device = window.device.uuid;
@@ -13,10 +14,12 @@ export default {
       device = "stubbed";
     }
 
+    //stub record
     user = store.createRecord('user', {
       'device_uuid': device
     });
 
+    //API will create or return existing User
     user.save().then(function(currentUser) {
       var appCon = container.lookup('controller:application');
       appCon.set('currentUser', currentUser);
--- app/mixins/current-user.js
+++ app/mixins/current-user.js
@@ -1,3 +1,6 @@
+//Mix me in to controllers to access currentUser
+//TODO - I will probably be deprecated for computed aliases
+
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
@@ -7,12 +10,20 @@ export default Ember.Mixin.create({
 
   registeredUser: function() {
     var user = this.currentUser;
-    if (!user) { return false; }
+    return true;
+    //if (!user) { return false; }
 
-    if (user.get('registered') === true) {
-      return true;
-    } else {
-      return false;
-    }
+    //if (user.get('registered') === true) {
+    //  return true;
+    //} else {
+    //  return false;
+    //}
   },
+
+  isAdmin: function() {
+    console.log('checking');
+    var user = this.registeredUser;
+    if (!user) return false;
+    console.log('still');
+  }.property()
 });
