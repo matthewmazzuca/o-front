8c3bc78da9fa4eda6e1f5ae621fe2b381c870b5e
gnazarkin <gleb.nazarkin@gmail.com>
2015-06-17 19:24:37 -0400

    Ember modal package

--- app/controllers/properties/property/index.js
+++ app/controllers/properties/property/index.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	needs: ['application']
+	needs: ['application'],
+
+	
+	isShowingModal: false,
+  actions: {
+    toggleModal: function(){
+      this.toggleProperty('isShowingModal');
+    }
+  }
 });
