278f3124d4cc109f9e7e0959d2eb6f510dff50b0
Matthew Mazzuca <matthew.mazzuca@mail.utoronto.ca>
2015-06-17 20:33:41 -0400

    minor changes for debugging

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
-import OpenHouseAPI from 'openhouse-front/mixins/adapters/open-house-api';
+import OpenhouseAPI from 'openhouse-front/mixins/adapters/open-house-api';
 
 export default DS.ActiveModelAdapter.extend(
-  OpenHouseAPI
+  OpenhouseAPI
 );
