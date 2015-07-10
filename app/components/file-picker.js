fab22937f3bb62a7b825235511a4ae7a6dfd8618
gnazarkin <gleb.nazarkin@gmail.com>
2015-07-10 17:20:24 -0400

    Updated verison and implemented minimum image size

--- app/components/file-picker.js
+++ app/components/file-picker.js
@@ -6,9 +6,10 @@ export default Ember.Component.extend({
 	openPicker: null,
 
 	pickerOptions: {
+    imageMin: [600, 600],
     mimetypes: 'image/*',
     container: 'window',
-    services: ['COMPUTER', 'IMAGE_SEARCH', 'WEBCAM', 'FACEBOOK', 'GMAIL', 'BOX', 'DROPBOX', 'FLICKR', 'PICASA', 'INSTAGRAM']
+    services: ['COMPUTER', 'IMAGE_SEARCH', 'WEBCAM', 'FACEBOOK', 'GMAIL', 'BOX', 'DROPBOX', 'FLICKR', 'PICASA', 'INSTAGRAM'],
   },
 
 	actions: {
@@ -18,8 +19,9 @@ export default Ember.Component.extend({
 
     fileSelected: function(InkBlob){
       var model = this.get('model');
-
+      console.log(InkBlob)
       this.set('model.image_url', InkBlob.url)
+      console.log(this.get('model.image_url'))
     },
 
     onClose: function () {
