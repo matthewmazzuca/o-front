817bd2690a504fa611fb890d7be237629f35f5b8
Joey Gollish <joey@addolabs.co>
2015-07-15 12:29:39 -0400

    styling changes to property edit button

--- app/components/file-picker.js
+++ app/components/file-picker.js
@@ -6,9 +6,9 @@ export default Ember.Component.extend({
 	openPicker: null,
 
 	pickerOptions: {
-    imageMin: [375, 240],
-    imageMax: [1000,1000],
-    imageQuality: 100,
+    // imageMin: [375, 240],
+    // imageMax: [1000,1000],
+    // imageQuality: 100,
     mimetypes: 'image/*',
     container: 'modal',
     services: ['COMPUTER', 'IMAGE_SEARCH', 'WEBCAM', 'FACEBOOK', 'GMAIL', 'BOX', 'DROPBOX', 'FLICKR', 'PICASA', 'INSTAGRAM'],
